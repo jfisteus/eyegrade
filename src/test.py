428c8718e007120c686d71baa90696b332e65f2d
Jesus Arias Fisteus <jaf@it.uc3m.es>
2010-05-14 01:09:59 +0200

    Original test.py for opencv found in the Internet.
    http://www.jperla.com/blog/2007/09/26/capturing-frames-from-a-webcam-on-linux/

---
+++ src/test.py
@@ -0,0 +1,33 @@
+import pygame
+import Image
+from pygame.locals import *
+import sys
+
+import opencv
+#this is important for capturing/displaying images
+from opencv import highgui 
+
+camera = highgui.cvCreateCameraCapture(1)
+def get_image():
+    im = highgui.cvQueryFrame(camera)
+    # Add the line below if you need it (Ubuntu 8.04+)
+    #im = opencv.cvGetMat(im)
+    #convert Ipl image to PIL image
+    return opencv.adaptors.Ipl2PIL(im) 
+
+fps = 30.0
+pygame.init()
+window = pygame.display.set_mode((640,480))
+pygame.display.set_caption("WebCam Demo")
+screen = pygame.display.get_surface()
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT or event.type == KEYDOWN:
+            sys.exit(0)
+    im = get_image()
+    pg_img = pygame.image.frombuffer(im.tostring(), im.size, im.mode)
+    screen.blit(pg_img, (0,0))
+    pygame.display.flip()
+    pygame.time.delay(int(1000 * 1.0/fps))
