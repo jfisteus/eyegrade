85016b06f2ce71aae89a0881c7b3c03db3ceb2d1
Jesus Arias Fisteus <jaf@it.uc3m.es>
2010-08-20 00:33:27 +0200

    Implemented the second part of the OCR algorithm. First complete version.
    Needs further testing and adjustments in the regular expressions, but
    doesn't work very bad.

--- src/ocr.py
+++ src/ocr.py
@@ -6,36 +6,61 @@ import tre
 from geometry import *
 import imageproc
 
-param_cross_num_lines = 13
+param_cross_num_lines = 15
 param_cell_margin = 2
 param_max_errors = 4
 
 # Tre initializations
 tre_fz = tre.Fuzzyness(maxerr = param_max_errors)
 regexps = [(r'^1{0,2}222+1{0,2}$', # zero
-            r'^1{0,2}222+1{0,2}$'),
-           (r'^11{0,3}(22+1+|211+|111+)11+$', # one
-            r'^1+|1{0,2}22+11+$'),
+            r'^1{0,2}222+1{0,2}$',
+            r'^/(.X./)+X_X/(X_X/)+(.X./)+$',
+            r'^/(.X./)+X_X/(X_X/)+(.X./)+$'),
+           (r'^11{0,4}(22+1+|211+|111+)1+$', # one
+            r'^1+|1{0,2}22+11+$',
+            r'^/(_.X/|_X./)(_.X/|_X./)+(X.X/)*(_X_/|__X/|XX_/)+'
+            + r'(.XX/|XX./){0,2}$',
+            r'(XXX/|XX_/_XX)'),
            (r'^11?2+111+2{0,2}11{0,2}$', # two
-            r'^1{0,3}2{0,2}(2|3)(2|3)(2|3)+11{0,2}$'),
+            r'^1{0,3}2{0,2}(2|3)(2|3)(2|3)+11{0,2}$',
+            r'^/(.X./)+(.../)*(_X./)+(X._/)*(X_./)+(.XX/|XX./)+(.../){0,2}$',
+            r'^/(._X/|X_./)+(XXX/)+(._X/|X_./)+(._./)*$'),
            (r'^1+2{0,2}11+2?11+2{0,2}1+$', # three
-            r'^1?22+33?(44?2+)?2{0,2}1{0,2}$'),
+            r'^1?22+33+(44?2+)?2{0,2}1{0,2}$',
+            r'^/(.../)(.../){0,2}(_.X/|_X./)+(..X/)*(XX./|.XX/)+(..X/)*'
+            + r'(_.X/)+(.../)(.../){0,2}$',
+            r'^/(._X/|X_./|_X./)*(XXX/)(XXX/)+(X../|.X./|..X/)*$'),
            (r'^1{0,2}22+111+$', # four
-            r'^11?(22?1|11+)(22?|1+)1+$'),
+            r'^11?(22?1|11+)(22?|1+)1+$',
+            r'^/(X_./|._X/)(X_./|._X/)+(XX./|.XX/)+(..X/)+(.X./){0,3}$',
+            r'^/(X._/|.X_/)+(.X./)(.X./)+(.XX/|XX./)+$'),
            (r'^111+2{0,2}11+2{0,2}1+$', # five
-            r'^1?2{0,2}333+2{0,2}1{0,2}$'),
+            r'^1?2{0,2}333+2{0,2}1{0,2}$',
+            r'^/(.XX/|XX./)+(X__/)+(XX./)+(__X/)+(X_X/)*(.XX/|XX./)+$',
+            r'^/(.../)+(XXX/)(XXX/)+(X../|.XX/)+$'),
            (r'^1{0,2}2{0,3}111+2{0,2}3{0,2}22+1{0,2}$', # six
-            r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$'),
+            r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$',
+            r'^/(.../)*(.X_/)*(X._/)+(.X./)+(XX./|.XX/)*(X.X/)+(.X./)+$',
+            r'^/(.X./|..X/)+(XXX/)+(.../){0,3}$'),
            (r'11?(22{0,2}|1+)2?1+2?11+', # seven
-            r'1+2(2|1)+11?'),
+            r'1+2(2|1)+11?',
+            r'^/(XX./|.XX/)+(_X./|_.X/)+(XX./){0,2}(_X./|_.X/)'
+            + r'(_X./|_.X/)+(.X_/)*$',
+            r'^/(_X./)*(X._/)+(.XX/)+.*$'),
            (r'^1{0,2}22+1+22+1{0,2}$', # eight
-            r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$'),
+            r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$',
+            r'^/(.../)(.../){0,3}(X_X/)+(.../)*(.X_/|_X./)+(.../)*(X_X/)+',
+            r'(XXX/)(XXX/)(XXX/)'),
            (r'^1{0,2}22+111+$', # nine
-            r'^1{0,2}(2+3+2*|222+)1+$')]
+            r'^1{0,2}(2+3+2*|222+)1+$',
+            r'^/(.../)+(X_X/)+(_.X/|_X./)(_.X/|_X./)+$',
+            r'^/(X._/|.X_/)(X._/|.X_/)+(XX./|.XX/)+$')]
 re_compiled = []
-for pair in regexps:
-    re_compiled.append((tre.compile(pair[0], tre.EXTENDED),
-                        tre.compile(pair[1], tre.EXTENDED)))
+for row in regexps:
+    re_compiled.append((tre.compile(row[0], tre.EXTENDED),
+                        tre.compile(row[1], tre.EXTENDED),
+                        tre.compile(row[2], tre.EXTENDED),
+                        tre.compile(row[3], tre.EXTENDED)))
 
 # limits: for each digit (min_len_num_hcrossings, min_len_num_vcrossings,
 #                         max_num_hcrossings, max_num_vcrossings,
@@ -44,7 +69,7 @@ limits = [(4, 4, 3, 3, 1, 1), # zero
           (4, 1, 3, 3, 1, 1), # one
           (4, 4, 3, 4, 1, 1), # two
           (4, 4, 3, 4, 1, 1), # three
-          (4, 4, 2, 2, 1, 1), # four
+          (4, 4, 3, 3, 1, 1), # four
           (4, 4, 3, 4, 1, 1), # five
           (4, 4, 3, 3, 1, 1), # six
           (4, 3, 3, 3, 1, 1), # seven
@@ -68,10 +93,11 @@ def digit_ocr_by_line_crossing(image, cell_corners, debug, image_drawn):
     hcrossings = []
     vcrossings = []
     for i in range(0, param_cross_num_lines):
+        h = float(i) / (param_cross_num_lines - 1)
         hcrossings.append(crossings(image, points_left[i], points_right[i],
-                                    debug, image_drawn))
+                                    h, debug, image_drawn))
         vcrossings.append(crossings(image, points_up[i], points_down[i],
-                                    debug, image_drawn))
+                                    h, debug, image_drawn))
     if debug:
         print hcrossings
         print vcrossings
@@ -89,6 +115,8 @@ def decide_digit(hcrossings, vcrossings, debug = False):
             print num_vcrossings
         hstr = ''.join([str(v) for v in num_hcrossings])
         vstr = ''.join([str(v) for v in num_vcrossings])
+        signatures = crossings_signatures(hcrossings, vcrossings)
+        print signatures
         scores = []
         for i in range(0, 10):
             if min(num_hcrossings) < limits[i][4] \
@@ -99,13 +127,17 @@ def decide_digit(hcrossings, vcrossings, debug = False):
                     or len(num_vcrossings) < limits[i][1]:
                 p = 0.0
             else:
-                hmatch = re_compiled[i][0].search(hstr, tre_fz)
-                vmatch = re_compiled[i][1].search(vstr, tre_fz)
-                hscore = max(1.0 - 0.3 * hmatch.cost, 0.0) if hmatch else 0.0
-                vscore = max(1.0 - 0.3 * vmatch.cost, 0.0) if vmatch else 0.0
+                hnmatch = re_compiled[i][0].search(hstr, tre_fz)
+                vnmatch = re_compiled[i][1].search(vstr, tre_fz)
+                hnscore = max(1.0 - 0.2 * hnmatch.cost if hnmatch else 0.3, 0.3)
+                vnscore = max(1.0 - 0.2 * vnmatch.cost if vnmatch else 0.3, 0.3)
+                hpmatch = re_compiled[i][2].search(signatures[0], tre_fz)
+                vpmatch = re_compiled[i][3].search(signatures[1], tre_fz)
+                hpscore = max(1.0 - 0.2 * hpmatch.cost if hpmatch else 0.5, 0.5)
+                vpscore = max(1.0 - 0.2 * vpmatch.cost if vpmatch else 0.5, 0.5)
                 if debug:
-                    print i, hscore, vscore
-                p = hscore * vscore
+                    print i, hnscore, vnscore, hpscore, vpscore
+                p = hnscore * vnscore * hpscore * vpscore
             scores.append((p, i))
         if debug:
             print sorted(scores, reverse = True)
@@ -114,7 +146,7 @@ def decide_digit(hcrossings, vcrossings, debug = False):
             decision = m[1]
     return decision
 
-def crossings(image, p0, p1, debug = False, image_drawn = None):
+def crossings(image, p0, p1, h, debug = False, image_drawn = None):
     pixels = []
     crossings = []
     for x, y in walk_line(p0, p1):
@@ -139,15 +171,49 @@ def crossings(image, p0, p1, debug = False, image_drawn = None):
                 begin = i
         else:
             if not value or i == len(pixels) - 1:
-                if i - begin > 1:
-                    pos = float(begin + i - 1) * 50 / len(pixels)
-                    length = float(i - begin) * 100 / len(pixels)
-                    crossings.append((pos, length))
+                end = i if value else i - 1
+                if end - begin > 0:
+                    n = len(pixels)
+                    begin_rel = float(begin) / n
+                    end_rel = float(end) / n
+                    center_rel = (begin_rel + end_rel) / 2
+                    length = end - begin + 1
+                    length_rel = float(length) / n
+                    crossings.append((begin_rel, end_rel,
+                                      center_rel, length, h))
                 begin = None
     return crossings
 
 # Other auxiliary functions
 #
+def crossings_signatures(hcrossings, vcrossings):
+#    min_length_px = min([val[3] for val in hcrossings + vcrossings])
+    min_v = hcrossings[0][0][4]
+    max_v = hcrossings[-1][0][4]
+    min_h = vcrossings[0][0][4]
+    max_h = vcrossings[-1][0][4]
+    signatures = []
+    for crossings, min_pos, max_pos \
+            in (hcrossings, min_h, max_h), (vcrossings, min_v, max_v):
+#        min_pos = min([min([v[0] for v in c]) for c in crossings if len(c) > 0])
+#        max_pos = max([max([v[1] for v in c]) for c in crossings if len(c) > 0])
+        region_width = (max_pos - min_pos) / 3
+        if region_width < 0.1:
+            region_width = 0.1
+        limits = (max_pos - 2 * region_width, max_pos - region_width)
+#        print ">>>", min_pos, max_pos, region_width, limits
+        particles = ['']
+        for row in crossings:
+            mark = [False, False, False]
+            for c in row:
+                mark[0] = mark[0] or c[0] < limits[0]
+                mark[1] = mark[1] or (c[0] < limits[1] and c[1] >= limits[0])
+                mark[2] = mark[2] or c[1] >= limits[1]
+            particles.append(''.join(['X' if m else '_' for m in mark]))
+        particles.append('')
+        signatures.append('/'.join(particles))
+    return signatures
+
 def __trim_empty_lists(lists):
     """Receives a list of lists. Returns a new list of lists without
        any empty lists at the beginning or end of it."""
@@ -160,6 +226,17 @@ def __trim_empty_lists(lists):
             end = i + 1
             if begin == -1:
                 begin = i
+    # Filter noise at the borders of the cell
+    if len(lists[begin]) == 1 and len(lists[begin + 1]) == 0 \
+            and lists[begin][0][3] < 4:
+        begin += 1
+        while begin < end and len(lists[begin]) == 0:
+            begin += 1
+    if len(lists[end - 1]) == 1 and len(lists[end - 2]) == 0 \
+            and lists[end - 1][0][3] < 4:
+        end -= 1
+        while end >= 1 and len(lists[end - 1]) == 0:
+            end -= 1
     return lists[begin:end]
 
 def adjust_cell_corners(image, corners):
