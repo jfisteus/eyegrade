e4604d499a6e0213b2b63a69a08cd6f5aa9b0940
Jesus Arias Fisteus <jaf@it.uc3m.es>
2010-08-21 03:07:17 +0200

    Improvements (tuning) the OCR module.
    Works better, but finer detection is needed.

--- src/ocr.py
+++ src/ocr.py
@@ -14,46 +14,50 @@ param_max_errors = 4
 tre_fz = tre.Fuzzyness(maxerr = param_max_errors)
 regexps = [(r'^1{0,2}222+1{0,2}$', # zero
             r'^1{0,2}222+1{0,2}$',
-            r'^/(.X./)+X_X/(X_X/)+(.X./)+$',
-            r'^/(.X./)+X_X/(X_X/)+(.X./)+$'),
-           (r'^11{0,4}(22+1+|211+|111+)1+$', # one
-            r'^1+|1{0,2}22+11+$',
+            r'^/(XXX/|X._/|_.X/|.X./)+X_X/(X_X/)+(XXX/|X._/|_.X/|.X./)+$',
+            r'^/(XXX/|X._/|_.X/|.X./)+X_X/(X_X/)+(XXX/|X._/|_.X/|.X./)+$'),
+           (r'^11+(22+1+|211+|111+)11+$', # one
+            r'^1+$|^1{0,2}2+11+$',
             r'^/(_.X/|_X./)(_.X/|_X./)+(X.X/)*(_X_/|__X/|XX_/)+'
             + r'(.XX/|XX./){0,2}$',
             r'(XXX/|XX_/_XX)'),
-           (r'^11?2+111+2{0,2}11{0,2}$', # two
-            r'^1{0,3}2{0,2}(2|3)(2|3)(2|3)+11{0,2}$',
-            r'^/(.X./)+(.../)*(_X./)+(X._/)*(X_./)+(.XX/|XX./)+(.../){0,2}$',
+           (r'^1+2{0,2}111+2{0,2}11{0,2}$', # two
+            r'^1{0,3}2*3+2+1{0,3}$',
+            r'^/(.../){0,3}(_X./)+(X._/)+(.XX/|XX./|X_X/)+(.../){0,2}$',
             r'^/(._X/|X_./)+(XXX/)+(._X/|X_./)+(._./)*$'),
            (r'^1+2{0,2}11+2?11+2{0,2}1+$', # three
-            r'^1?22+33+(44?2+)?2{0,2}1{0,2}$',
-            r'^/(.../)(.../){0,2}(_.X/|_X./)+(..X/)*(XX./|.XX/)+(..X/)*'
-            + r'(_.X/)+(.../)(.../){0,2}$',
-            r'^/(._X/|X_./|_X./)*(XXX/)(XXX/)+(X../|.X./|..X/)*$'),
+            r'^1*(22+3+|2+33+)(44?2+)?2{0,2}1{0,2}$',
+            r'^/(X._/|_.X|/XXX/)(X._/|_.X|/XXX/){0,2}(X_X){0,2}(_.X/|_X./)+'
+            + r'(XX./|.XX/)+(_.X/)+(X_X){0,2}(X._/|_.X|/XXX/)'
+            + r'(X._/|_.X|/XXX/){0,2}$',
+            r'^/(._X/|X_./)*(XXX/)(XXX/)+(X../|.X./|..X/)*$'),
            (r'^1{0,2}22+111+$', # four
             r'^11?(22?1|11+)(22?|1+)1+$',
-            r'^/(X_./|._X/)(X_./|._X/)+(XX./|.XX/)+(..X/)+(.X./){0,3}$',
+            r'^/(X_./|._X/)(X_./|._X/)+(X_X/)+(XX./|.XX/)+(..X/)+(.X./){0,3}$',
             r'^/(X._/|.X_/)+(.X./)(.X./)+(.XX/|XX./)+$'),
            (r'^111+2{0,2}11+2{0,2}1+$', # five
-            r'^1?2{0,2}333+2{0,2}1{0,2}$',
-            r'^/(.XX/|XX./)+(X__/)+(XX./)+(__X/)+(X_X/)*(.XX/|XX./)+$',
+            r'^1?2{0,2}(2|3)33+(1|2){0,4}$',
+            r'^/(XX./|.XX/|X._/|_.X/)+(X__/)+(XX./|.XX/){0,3}(__X/)+'
+            + r'(X_X/)*(.XX/|XX./|X__/|__X/)+$',
             r'^/(.../)+(XXX/)(XXX/)+(X../|.XX/)+$'),
            (r'^1{0,2}2{0,3}111+2{0,2}3{0,2}22+1{0,2}$', # six
             r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$',
-            r'^/(.../)*(.X_/)*(X._/)+(.X./)+(XX./|.XX/)*(X.X/)+(.X./)+$',
+            r'^/(.../){0,3}(X__/|_X_/)(X__/|_X_/)+(X._/)+(.../)(.../){0,2}'
+            + '(X_X/)+(.../)(.../){0,2}$',
             r'^/(.X./|..X/)+(XXX/)+(.../){0,3}$'),
            (r'11?(22{0,2}|1+)2?1+2?11+', # seven
             r'1+2(2|1)+11?',
-            r'^/(XX./|.XX/)+(_X./|_.X/)+(XX./){0,2}(_X./|_.X/)'
-            + r'(_X./|_.X/)+(.X_/)*$',
+            r'^/(X../|..X/){0,3}(_X./|_.X/)+(.XX/)(.XX/)?(_X./|_.X/)'
+            + r'(_X./|_.X/)+(.X_/|X._/)*$',
             r'^/(_X./)*(X._/)+(.XX/)+.*$'),
            (r'^1{0,2}22+1+22+1{0,2}$', # eight
             r'^1{0,2}2{0,2}333+2{0,2}1{0,2}$',
-            r'^/(.../)(.../){0,3}(X_X/)+(.../)*(.X_/|_X./)+(.../)*(X_X/)+',
+            r'^/(.../)(.../){0,3}(X_X/)+(.../)*(.X_/|_X./|X__/|__X/)+'
+            + r'(.../)*(X_X/)+',
             r'(XXX/)(XXX/)(XXX/)'),
-           (r'^1{0,2}22+111+$', # nine
+           (r'^1{0,2}22+3?2?12?11+$', # nine
             r'^1{0,2}(2+3+2*|222+)1+$',
-            r'^/(.../)+(X_X/)+(_.X/|_X./)(_.X/|_X./)+$',
+            r'^/(.../)+(X_X/)+(_XX/|XXX/)(_XX/|XXX/)?(_.X/)+(_X./)*$',
             r'^/(X._/|.X_/)(X._/|.X_/)+(XX./|.XX/)+$')]
 re_compiled = []
 for row in regexps:
@@ -64,17 +68,18 @@ for row in regexps:
 
 # limits: for each digit (min_len_num_hcrossings, min_len_num_vcrossings,
 #                         max_num_hcrossings, max_num_vcrossings,
-#                         min_num_hcrossings, min_num_vcrossings)
-limits = [(4, 4, 3, 3, 1, 1), # zero
-          (4, 1, 3, 3, 1, 1), # one
-          (4, 4, 3, 4, 1, 1), # two
-          (4, 4, 3, 4, 1, 1), # three
-          (4, 4, 3, 3, 1, 1), # four
-          (4, 4, 3, 4, 1, 1), # five
-          (4, 4, 3, 3, 1, 1), # six
-          (4, 3, 3, 3, 1, 1), # seven
-          (4, 4, 3, 3, 1, 1), # eight
-          (4, 3, 3, 3, 1, 1)] # nine
+#                         min_num_hcrossings, min_num_vcrossings,
+#                         min_max_num_hcrossings, min_max_num_vcrossings)
+limits = [(4, 4, 3, 3, 1, 1, 2, 2), # zero
+          (4, 1, 2, 2, 1, 1, 1, 1), # one
+          (4, 4, 3, 3, 1, 1, 1, 3), # two
+          (4, 4, 2, 4, 1, 1, 1, 3), # three
+          (4, 4, 3, 2, 1, 1, 2, 1), # four
+          (4, 4, 2, 3, 1, 1, 1, 3), # five
+          (4, 4, 2, 3, 1, 1, 2, 2), # six
+          (4, 3, 2, 3, 1, 1, 1, 2), # seven
+          (4, 4, 2, 4, 1, 1, 2, 3), # eight
+          (4, 3, 2, 3, 1, 1, 2, 2)] # nine
 
 def digit_ocr(image, cell_corners, debug = None, image_drawn = None):
     assert(not debug or image_drawn is not None)
@@ -119,25 +124,32 @@ def decide_digit(hcrossings, vcrossings, debug = False):
         print signatures
         scores = []
         for i in range(0, 10):
+            max_num_hcrossings = max(num_hcrossings)
+            max_num_vcrossings = max(num_vcrossings)
             if min(num_hcrossings) < limits[i][4] \
                     or min(num_vcrossings) < limits[i][5] \
-                    or max(num_hcrossings) > limits[i][2] \
-                    or max(num_vcrossings) > limits[i][3] \
                     or len(num_hcrossings) < limits[i][0] \
                     or len(num_vcrossings) < limits[i][1]:
                 p = 0.0
             else:
+                mhcscore = min_max_crossings_score(limits[i][6], limits[i][2],
+                                                   max_num_hcrossings)
+                mvcscore = min_max_crossings_score(limits[i][7], limits[i][3],
+                                                   max_num_vcrossings)
                 hnmatch = re_compiled[i][0].search(hstr, tre_fz)
                 vnmatch = re_compiled[i][1].search(vstr, tre_fz)
-                hnscore = max(1.0 - 0.2 * hnmatch.cost if hnmatch else 0.3, 0.3)
-                vnscore = max(1.0 - 0.2 * vnmatch.cost if vnmatch else 0.3, 0.3)
+                hnscore = max(1.0 - 0.25 * hnmatch.cost \
+                                  if hnmatch else 0.2, 0.2)
+                vnscore = max(1.0 - 0.25 * vnmatch.cost \
+                                  if vnmatch else 0.2, 0.2)
                 hpmatch = re_compiled[i][2].search(signatures[0], tre_fz)
                 vpmatch = re_compiled[i][3].search(signatures[1], tre_fz)
-                hpscore = max(1.0 - 0.2 * hpmatch.cost if hpmatch else 0.5, 0.5)
-                vpscore = max(1.0 - 0.2 * vpmatch.cost if vpmatch else 0.5, 0.5)
+                hpscore = max(1.0 - 0.2 * hpmatch.cost if hpmatch else 0.4, 0.4)
+                vpscore = max(1.0 - 0.2 * vpmatch.cost if vpmatch else 0.4, 0.4)
                 if debug:
-                    print i, hnscore, vnscore, hpscore, vpscore
-                p = hnscore * vnscore * hpscore * vpscore
+                    print i, hnscore, vnscore, hpscore, vpscore, \
+                        mhcscore, mvcscore
+                p = hnscore * vnscore * hpscore * vpscore * mhcscore * mvcscore
             scores.append((p, i))
         if debug:
             print sorted(scores, reverse = True)
@@ -190,9 +202,7 @@ def crossings_signatures(hcrossings, vcrossings):
     min_length_px = min([min([v[3] for v in c]) \
                              for c in hcrossings + vcrossings \
                              if len(c) > 0])
-    width_threshold = max(2.0 * min_length_px, 16.0)
-    if min_length_px < 8:
-        min_length_px = 8
+    width_threshold = 3 * min_length_px
     min_v = hcrossings[0][0][4]
     max_v = hcrossings[-1][0][4]
     min_h = vcrossings[0][0][4]
@@ -200,13 +210,11 @@ def crossings_signatures(hcrossings, vcrossings):
     signatures = []
     for crossings, min_pos, max_pos \
             in (hcrossings, min_h, max_h), (vcrossings, min_v, max_v):
-#        min_pos = min([min([v[0] for v in c]) for c in crossings if len(c) > 0])
-#        max_pos = max([max([v[1] for v in c]) for c in crossings if len(c) > 0])
         region_width = (max_pos - min_pos) / 3
         if region_width < 0.1:
             region_width = 0.1
         limits = (max_pos - 2 * region_width, max_pos - region_width)
-#        print ">>>", min_pos, max_pos, region_width, limits
+        print ">>>", min_pos, max_pos, region_width, limits, width_threshold
         particles = ['']
         for row in crossings:
             mark = [False, False, False]
@@ -215,8 +223,9 @@ def crossings_signatures(hcrossings, vcrossings):
                 m[0] = c[0] < limits[0]
                 m[1] = (c[0] < limits[1] and c[1] >= limits[0])
                 m[2] = c[1] >= limits[1]
-                if c[3] <= width_threshold \
-                        and (c[2] < limits[0] or c[2] >= limits[1]):
+                if len(row) <= 2 and c[3] <= width_threshold \
+                        and c[1] - c[0] < region_width \
+                        and (c[0] < limits[0] or c[1] >= limits[1]):
                     m[1] = False
                 for i in range(3):
                     mark[i] = mark[i] or m[i]
@@ -238,17 +247,37 @@ def __trim_empty_lists(lists):
             if begin == -1:
                 begin = i
     # Filter noise at the borders of the cell
-    if len(lists[begin]) == 1 and len(lists[begin + 1]) == 0 \
-            and lists[begin][0][3] < 4:
+    if (len(lists[begin]) == 1 and end - begin > 1 \
+            and len(lists[begin + 1]) == 0 and lists[begin][0][3] < 6) \
+            or (end - begin > 2 and len(lists[begin + 1]) == 0 \
+                    and len(lists[begin + 2]) == 0):
         begin += 1
         while begin < end and len(lists[begin]) == 0:
             begin += 1
-    if len(lists[end - 1]) == 1 and len(lists[end - 2]) == 0 \
-            and lists[end - 1][0][3] < 4:
+    if (len(lists[end - 1]) == 1 and end >= 2 and len(lists[end - 2]) == 0 \
+            and lists[end - 1][0][3] < 6) \
+            or (end >= 3 and len(lists[end - 2]) == 0 \
+                    and len(lists[end - 3]) == 0):
         end -= 1
         while end >= 1 and len(lists[end - 1]) == 0:
             end -= 1
     return lists[begin:end]
+
+def min_max_crossings_score(min_max_crossings, max_max_crossings,
+                            actual_max_crossings):
+    d = min_max_crossings - actual_max_crossings
+    if d == 1:
+        score = 0.5
+    elif d > 1:
+        score = 0.1
+    else:
+        score = 1.0
+    d = max_max_crossings - actual_max_crossings
+    if d == -1:
+        score = score * 0.5
+    elif d < -1:
+        score = score * 0.1
+    return score
 
 def adjust_cell_corners(image, corners):
     plu, pru, pld, prd = corners
