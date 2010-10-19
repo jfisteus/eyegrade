0954cabd565874be0ad1dce01f8ecb2643c80c33
Jesus Arias Fisteus <jaf@it.uc3m.es>
2010-10-20 00:07:20 +0200

    Improved statistics in the analysis module. Now with main.

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,7 @@
 # Module for analysis of results
 #
 import csv
+import sys
 
 # Local imports
 import utils
@@ -23,13 +24,16 @@ def process_results(results, permutations = []):
             result['model'] = int(result['model'])
         else:
             result['model'] = ord(result['model']) - ord('A')
+        result['good'] = int(result['good'])
+        result['bad'] = int(result['bad'])
+        result['unknown'] = int(result['unknown'])
         answers = [int(n) for n in result['answers'].split('/')]
         if permutations != []:
             answers = __permute_answers(answers, permutations[result['model']])
         result['answers'] = answers
 
 def stats_for_question(results, question, num_options = None):
-    """Returns the tuple with the count of answers (blank, opt1,
+    """Returns a tuple with the count of answers (blank, opt1,
        opt2,...) for the specified question number. Use 0 for the
        first question (instead of 1). If num_options is provided,
        the resulting tuple will have num_options + 1 components, even
@@ -47,15 +51,51 @@ def stats_for_question(results, question, num_options = None):
 def stats_by_question(results, num_questions):
     return [stats_for_question(results, i) for i in range(0, num_questions)]
 
+def stats_for_model(results, model, num_questions):
+    """Returns a tuple with the count of (correct, incorrect, blank)
+       answers for the given model. Model is an integer, 0 for A, 1
+       for B, etc."""
+    data = [(r['good'], r['bad']) for r in results if r['model'] == model]
+    if data != []:
+        good = float(sum([d[0] for d in data])) / len(data)
+        bad = float(sum([d[1] for d in data])) / len(data)
+        blank = num_questions - good - bad
+    else:
+        good = 0
+        bad = 0
+        blank = 0
+    return (len(data), good, bad, blank)
+
+def stats_by_model(results, num_questions, num_models):
+    return [stats_for_model(results, i, num_questions) \
+                for i in range(0, num_models)]
+
 def print_stats_by_question(stats):
     for i, answers in enumerate(stats):
         count = sum(answers)
         print 'Question %d'%(i + 1)
         for j in range(1, len(answers)):
             percentage = float(answers[j]) * 100 / count
-            print '  - Option %d: %d (%.1f%%)'%(j, answers[j], percentage)
+            if answers[j] > 0:
+                data = (chr(64 + j), answers[j], percentage)
+                print '  - Option %s: %d (%.1f%%)'%data
         percentage = float(answers[0]) * 100 / count
-        print '  - Blank: %d (%.1f%%)'%(answers[0], percentage)
+        print '  - Not answered: %d (%.1f%%)'%(answers[0], percentage)
+
+def print_stats_by_model(stats):
+    for i, data in enumerate(stats):
+        num_questions = sum(data)
+        print 'Model %s; number of exams: %d'%(chr(65 + i), data[0])
+        if data[0] > 0:
+            if num_questions > 0:
+                percentages = (float(data[1]) * 100 / num_questions,
+                               float(data[2]) * 100 / num_questions,
+                               float(data[3]) * 100 / num_questions)
+            else:
+                percentages = (0, 0, 0)
+            print '    - Correct: %.1f (%.1f%%)'%(data[1], percentages[0])
+            print '    - Incorrect: %.1f (%.1f%%)'%(data[2], percentages[1])
+            print '    - Not answered: %.1f (%.1f%%)'%(data[3], percentages[2])
 
 def analyze(results_filename, exam_cfg_filename):
     exam_data = utils.ExamConfig(exam_cfg_filename)
@@ -63,6 +103,10 @@ def analyze(results_filename, exam_cfg_filename):
     process_results(results, exam_data.permutations)
     stats = stats_by_question(results, exam_data.num_questions)
     print_stats_by_question(stats)
+    print
+    stats = stats_by_model(results, exam_data.num_questions,
+                           exam_data.num_models)
+    print_stats_by_model(stats)
     return results
 
 def __permute_answers(answers, permutation):
@@ -75,3 +119,9 @@ def __permute_answers(answers, permutation):
             resolved_option = permutation[i][1][option - 1]
         permutted[permutation[i][0] - 1] = resolved_option
     return permutted
+
+def main():
+    analyze(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
