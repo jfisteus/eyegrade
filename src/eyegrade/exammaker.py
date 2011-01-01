68e8a14bf60482cab86cb679d5b00139414dc723
Jesus Arias Fisteus <jaf@it.uc3m.es>
2011-01-01 23:28:16 +0100

    Replacements in exammaker without regular expressions.

--- src/eyegrade/exammaker.py
+++ src/eyegrade/exammaker.py
@@ -9,10 +9,7 @@ param_min_num_questions = 1
 
 # Numbers of questions in which the number of tables is changed
 param_table_limits = [8, 24, 55]
-re_id_box = r'{{id-box\(([0-9]+),(.*)\)}}'
-re_id_box_full = '.*' + re_id_box + '.*'
-re_id_box_comp = re.compile(re_id_box)
-re_id_box_full_comp = re.compile(re_id_box_full, re.DOTALL)
+re_split_template = re.compile('{{([^{}]+)}}')
 
 class ExamMaker(object):
     def __init__(self, num_questions, num_choices, template_filename,
@@ -20,25 +17,17 @@ class ExamMaker(object):
         self.num_questions = num_questions
         self.num_choices = num_choices
         self.num_tables = num_tables
-        self.template = utils.read_file(template_filename)
+        template = utils.read_file(template_filename)
+        self.parts = re_split_template.split(template)
         self.output_file = output_file
-        self.variables = variables
         self.questions = None
-        id_label, self.id_num_digits = id_num_digits(self.template)
-        self.__load_replacements(id_label)
+        id_label, self.id_num_digits = id_num_digits(self.parts)
+        self.__load_replacements(variables, id_label)
 
     def set_questions(self, questions):
         if len(questions) != self.num_questions:
             raise Exception('Incorrect number of questions')
         self.questions = questions
-
-    def __load_replacements(self, id_label):
-        self.replacements = {}
-        for var in self.variables:
-            self.replacements[re.compile('{{' + var + '}}')] = \
-                self.variables[var]
-        self.replacements[re_id_box_comp] = create_id_box(id_label,
-                                                          self.id_num_digits)
 
     def create_exam(self, model):
         if model is None or len(model) != 1 or ord(model) < 65 or \
@@ -47,15 +36,32 @@ class ExamMaker(object):
         answer_table = create_answer_table(self.num_questions, self.num_choices,
                                            model, self.num_tables)
         replacements = copy.copy(self.replacements)
-        replacements[re.compile('{{answer-table}}')] = answer_table
-        replacements[re.compile('{{model}}')] = model
-        exam_text = self.template
-        for exp in replacements:
-            exam_text = exp.sub(replacements[exp], exam_text)
+        replacements['answer-table'] = answer_table
+        replacements['model'] = model
+        # Replacement keys are in odd positions of self.parts
+        replaced = len(self.parts) * [None]
+        replaced[::2] = self.parts[::2]
+        replaced[1::2] = [self.__replace(key, replacements) \
+                              for key in self.parts[1::2]]
+        exam_text = ''.join(replaced)
         if self.output_file == sys.stdout:
             utils.write_to_stdout(exam_text)
         else:
             utils.write_file(self.output_file%model, exam_text)
+
+    def __load_replacements(self, variables, id_label):
+        self.replacements = copy.copy(variables)
+        self.replacements['id-box'] = create_id_box(id_label,
+                                                    self.id_num_digits)
+        self.replacements['questions'] = ''
+
+    def __replace(self, key, replacements):
+        if key in replacements:
+            return replacements[key]
+        elif key.startswith('id-box'):
+            return replacements['id-box']
+        else:
+            raise Exception('Unknown replacement key: ' + key)
 
 def create_answer_table(num_questions, num_choices, model, num_tables = 0):
     """Returns a string with the answer tables of the asnwer sheet.
@@ -86,8 +92,8 @@ def create_answer_table(num_questions, num_choices, model, num_tables = 0):
         rows.append(__horizontal_line(row_geometry, num_choices, compact))
         rows.append(__build_row(i, row_geometry, question_numbers,
                                 num_choices, bits_rows, compact))
-    rows.append('\\end{tabular}')
-    rows.append('\\end{center}')
+    rows.append(r'\end{tabular}')
+    rows.append(r'\end{center}')
     return '\n'.join(rows)
 
 
@@ -95,32 +101,31 @@ def create_id_box(label, num_digits):
     """Creates the ID box given a label to show and number of digits.
 
     """
-    parts = ['\\\\begin{center}', '\Large']
-    parts.append('\\\\begin{tabular}{l|' + num_digits * 'p{3mm}|' + '}')
-    parts.append('\\cline{2-%d}'%(1 + num_digits))
-    parts.append('\\\\textbf{%s}: '%label + num_digits * '& ' + '\\\\\\\\')
-    parts.append('\\cline{2-%d}'%(1 + num_digits))
-    parts.append('\\end{tabular}')
-    parts.append('\\end{center}')
+    parts = [r'\begin{center}', r'\Large']
+    parts.append(r'\begin{tabular}{l|' + num_digits * 'p{3mm}|' + '}')
+    parts.append(r'\cline{2-%d}'%(1 + num_digits))
+    parts.append(r'\textbf{%s}: '%label + num_digits * '& ' + r'\\')
+    parts.append(r'\cline{2-%d}'%(1 + num_digits))
+    parts.append(r'\end{tabular}')
+    parts.append(r'\end{center}')
     return '\n'.join(parts)
 
-def id_num_digits(template):
+def id_num_digits(parts):
     """Returns the tuple (label, number of digits) for the ID box.
 
-       Receives the text of the template for the exam, which may
-       contain a key like '{{id-box(9,NIA)}}', in which NIA is the
-       label and 9 the number of digits. If the key does not exist in
-       the template, returns (0, None).
+       Receives the splitted text of the template for the exam, which
+       may contain a key like 'id-box(9,NIA)' in an odd position, in
+       which NIA is the label and 9 the number of digits. If the key
+       does not exist in the template, returns (0, None).
 
     """
-    if re_id_box_full_comp.match(template):
-        data = re_id_box_full_comp.sub(r'\1/\2', template).split('/')
-        num_digits = int(data[0])
-        label = data[1]
-    else:
-        num_digits = 0
-        label = None
-    return label, num_digits
+    # Replacement keys are in odd positions of the list
+    for part in parts[1::2]:
+        if part.startswith('id-box'):
+            data = part[7:-1].split(',')
+            # data[0] is num_digits; data[1] is label
+            return data[1], int(data[0])
+    return None, 0
 
 def __choose_num_tables(num_questions):
     """Returns a good number of tables for the given number of questions."""
@@ -171,7 +176,7 @@ def __horizontal_line(row_geometry, num_choices, compact):
     first = 2
     for i, geometry in enumerate(row_geometry):
         if geometry > 0 or geometry == -1:
-            parts.append('\\cline{%d-%d}'%(first, first + num_choices - 1))
+            parts.append(r'\cline{%d-%d}'%(first, first + num_choices - 1))
         first += 1 + num_empty_columns + num_choices
     return ' '.join(parts)
 
@@ -180,16 +185,16 @@ def __table_top(num_tables, num_choices, compact):
     middle_sep_header = ' & & ' if not compact else ' & '
     l = middle_sep_format.join(num_tables
                                * ['|'.join(['r'] + num_choices * ['c'] + [''])])
-    l = '\\\\begin{tabular}{' + l + '}'
-    lines = ['\\\\begin{center}', '\\large', l]
+    l = r'\begin{tabular}{' + l + '}'
+    lines = [r'\begin{center}', r'\large', l]
     parts = []
     for i in range(0, num_tables):
         parts_internal = []
-        parts_internal.append('\\multicolumn{1}{c}{}')
+        parts_internal.append(r'\multicolumn{1}{c}{}')
         for j in range(0, num_choices):
-            parts_internal.append('\\multicolumn{1}{c}{%s}'%chr(65 + j))
+            parts_internal.append(r'\multicolumn{1}{c}{%s}'%chr(65 + j))
         parts.append(' & '.join(parts_internal))
-    lines.append(middle_sep_header.join(parts) + ' \\\\\\\\')
+    lines.append(middle_sep_header.join(parts) + r' \\')
     return lines
 
 def __build_row(num_row, row_geometry, question_numbers, num_choices,
@@ -209,18 +214,18 @@ def __build_row(num_row, row_geometry, question_numbers, num_choices,
             skip_cells += 1 + num_empty_columns + num_choices
     row = ' & & '.join(parts) if not compact else ' & '.join(parts)
     if skip_cells > 0:
-        row += ' & \\multicolumn{%d}{c}{}'%skip_cells
-    return row + ' \\\\\\\\'
+        row += r' & \multicolumn{%d}{c}{}'%skip_cells
+    return row + r' \\'
 
 def __build_question_cell(num_question, num_choices):
     parts = [str(num_question)]
     for i in range(0, num_choices):
-        parts.append('\\light{%s}'%chr(65 + i))
+        parts.append(r'\light{%s}'%chr(65 + i))
     return ' & '.join(parts)
 
 def __create_infobits(bits, num_tables, num_choices):
-    column_active = '\\multicolumn{1}{c}{$\\\\blacksquare$}'
-    column_inactive = '\\multicolumn{1}{c}{}'
+    column_active = r'\multicolumn{1}{c}{$\blacksquare$}'
+    column_inactive = r'\multicolumn{1}{c}{}'
     parts = [[], []]
     for i in range(0, num_tables):
         data = bits[i * num_choices: (i + 1) * num_choices]
