0c7f4be01738d47e39371e7dd2167602f8802b37
Jesus Arias Fisteus <jfisteus@gmail.com>
2012-12-14 01:21:13 +0100

    Create a menu and improvements in the toolbar.

--- src/eyegrade/qtgui/mainapp.py
+++ src/eyegrade/qtgui/mainapp.py
@@ -19,56 +19,120 @@
 #from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import (QImage, QWidget, QMainWindow, QPainter,
                          QSizePolicy, QApplication, QVBoxLayout,
-                         QLabel, QIcon, QAction,)
+                         QLabel, QIcon, QAction, QMenu,)
 
 from PyQt4.QtCore import Qt, QTimer
 
 from eyegrade.utils import resource_path
 
 
-class ToolbarManager(object):
+class ActionsManager(object):
     """Creates and manages the toolbar buttons."""
 
-    _action_data = [
-        ('snapshot', 'snapshot.svg', 'Snapshot'),
-        ('manual_detect', 'manual_detect.svg', 'Set exam bounds manually'),
-        ('next_id', 'next_id.svg', 'Next student id'),
-        ('edit_id', 'edit_id.svg', 'Edit student id'),
-        ('save', 'save.svg', 'Save the current exam'),
-        ('discard', 'discard.svg', 'Discard the current capture'),
-        ('exit', 'exit.svg', 'Exit'),
+    _actions_grading_data = [
+        ('snapshot', 'snapshot.svg', 'Sna&pshot'),
+        ('manual_detect', 'manual_detect.svg', '&Manual bounds'),
+        ('next_id', 'next_id.svg', '&Next student id'),
+        ('edit_id', 'edit_id.svg', '&Edit student id'),
+        ('save', 'save.svg', '&Save capture'),
+        ('discard', 'discard.svg', '&Discard capture'),
         ]
 
-    def __init__(self, toolbar, window):
+    _actions_session_data = [
+        ('new', 'new.svg', '&New session'),
+        ('open', 'open.svg', '&Open session'),
+        ('close', 'close.svg', '&Close session'),
+        ('*separator*', None, None),
+        ('exit', 'exit.svg', '&Exit'),
+        ]
+
+    _actions_help_data = [
+        ('about', None, '&About'),
+        ]
+
+    def __init__(self, window):
         """Creates a manager for the given toolbar object."""
-        self.toolbar = toolbar
         self.window = window
-        self.actions = {}
-        for key, icon, tooltip in ToolbarManager._action_data:
-            self._add_action(key, icon, tooltip)
+        self.menubar = window.menuBar()
+        self.toolbar = window.addToolBar('Grade Toolbar')
+        self.menus = {}
+        self.actions_grading = {}
+        self.actions_session = {}
+        action_lists = {'session': [], 'grading': []}
+        for key, icon, tooltip in ActionsManager._actions_session_data:
+            self._add_action(key, icon, tooltip, self.actions_session,
+                             action_lists['session'])
+        for key, icon, tooltip in ActionsManager._actions_grading_data:
+            self._add_action(key, icon, tooltip, self.actions_grading,
+                             action_lists['grading'])
+        self._populate_menubar(action_lists)
+        self._populate_toolbar(action_lists)
 
     def set_search_mode(self):
-        self.actions['snapshot'].setEnabled(True)
-        self.actions['manual_detect'].setEnabled(True)
-        self.actions['next_id'].setEnabled(False)
-        self.actions['edit_id'].setEnabled(False)
-        self.actions['save'].setEnabled(False)
-        self.actions['discard'].setEnabled(False)
-        self.actions['exit'].setEnabled(True)
+        self.actions_grading['snapshot'].setEnabled(True)
+        self.actions_grading['manual_detect'].setEnabled(True)
+        self.actions_grading['next_id'].setEnabled(False)
+        self.actions_grading['edit_id'].setEnabled(False)
+        self.actions_grading['save'].setEnabled(False)
+        self.actions_grading['discard'].setEnabled(False)
+        self.actions_session['close'].setEnabled(True)
+        self.menus['grading'].setEnabled(True)
 
     def set_review_mode(self):
-        self.actions['snapshot'].setEnabled(False)
-        self.actions['manual_detect'].setEnabled(False)
-        self.actions['next_id'].setEnabled(True)
-        self.actions['edit_id'].setEnabled(True)
-        self.actions['save'].setEnabled(True)
-        self.actions['discard'].setEnabled(True)
-        self.actions['exit'].setEnabled(True)
+        self.actions_grading['snapshot'].setEnabled(False)
+        self.actions_grading['manual_detect'].setEnabled(False)
+        self.actions_grading['next_id'].setEnabled(True)
+        self.actions_grading['edit_id'].setEnabled(True)
+        self.actions_grading['save'].setEnabled(True)
+        self.actions_grading['discard'].setEnabled(True)
+        self.actions_session['close'].setEnabled(True)
+        self.menus['grading'].setEnabled(True)
 
-    def _add_action(self, action_name, icon_file, tooltip):
-        action = QAction(QIcon(resource_path(icon_file)), tooltip, self.window)
-        self.actions[action_name] = action
-        self.toolbar.addAction(action)
+    def set_session_closed_mode(self):
+        for key in self.actions_grading:
+            self.actions_grading[key].setEnabled(False)
+        self.menus['grading'].setEnabled(False)
+
+    def _add_action(self, action_name, icon_file, tooltip, group, actions_list):
+        action = self._create_action(action_name, icon_file, tooltip)
+        if not action.isSeparator():
+            group[action_name] = action
+        actions_list.append(action)
+
+    def _populate_menu(self, menu, actions_data):
+        for key, icon, tooltip in actions_data:
+            menu.addAction(self._create_action(key, icon, tooltip))
+
+    def _create_action(self, action_name, icon_file, tooltip):
+        if action_name == '*separator*':
+            action = QAction(self.window)
+            action.setSeparator(True)
+        else:
+            if icon_file:
+                action = QAction(QIcon(resource_path(icon_file)),
+                                 tooltip, self.window)
+            else:
+                action = QAction(tooltip, self.window)
+        return action
+
+    def _populate_menubar(self, action_lists):
+        self.menus['session'] = QMenu('&Session', self.menubar)
+        self.menus['grading'] = QMenu('&Grading', self.menubar)
+        self.menubar.addMenu(self.menus['session'])
+        self.menubar.addMenu(self.menus['grading'])
+        for action in action_lists['session']:
+            self.menus['session'].addAction(action)
+        for action in action_lists['grading']:
+            self.menus['grading'].addAction(action)
+        help_menu = QMenu('&Help', self.menubar)
+        self.menubar.addMenu(help_menu)
+        self._populate_menu(help_menu, ActionsManager._actions_help_data)
+
+    def _populate_toolbar(self, action_lists):
+        for action in action_lists['grading']:
+            self.toolbar.addAction(action)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.actions_session['close'])
 
 
 class CamView(QWidget):
@@ -120,9 +184,8 @@ class MainWindow(QMainWindow):
         self.center_view = CenterView()
         self.setCentralWidget(self.center_view)
         self.setWindowTitle("Test cam")
-        toolbar = self.addToolBar('Eyegrade Toolbar')
-        self.toolbar_manager = ToolbarManager(toolbar, self)
-        self.toolbar_manager.set_search_mode()
+        self.actions_manager = ActionsManager(self)
+        self.actions_manager.set_search_mode()
         self.adjustSize()
         self.setFixedSize(self.sizeHint())
 
