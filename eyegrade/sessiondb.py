jfisteus/eyegrade

commit 84eaed329352f1334febfc5b0dd6d351a70ad04f

Fix an error when removing exams with no assigned student.

eyegrade/sessiondb.py
@@ -421,7 +421,11 @@ class SessionDB(object):
         cursor.execute('SELECT * FROM Students '
                        'INNER JOIN Exams ON Students.db_id = Exams.student '
                        'WHERE exam_id = ?', (exam_id,))
-        return _create_student_from_row(cursor.fetchone())
+        row = cursor.fetchone()
+        if row is not None:
+            return _create_student_from_row(cursor.fetchone())
+        else:
+            return None
 
     def _read_cell_geometries(self, exam_id, load_corners=False):
         if not load_corners:

commit b6ca7ea38e6da551da2921f8dfc49c1510d0bd56

Fix a bug introduced in commit 84eaed329352.

eyegrade/sessiondb.py
@@ -423,7 +423,7 @@ class SessionDB(object):
                        'WHERE exam_id = ?', (exam_id,))
         row = cursor.fetchone()
         if row is not None:
-            return _create_student_from_row(cursor.fetchone())
+            return _create_student_from_row(row)
         else:
             return None
 