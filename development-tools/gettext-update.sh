28a7c56ccb394863fc8fb811cd96d6636cb17250
Jesus Arias Fisteus <jfisteus@gmail.com>
2014-12-24 12:24:35 +0100

    Update language files for the new gui code layout.

--- development-tools/gettext-update.sh
+++ development-tools/gettext-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-srcfiles="eyegrade/eyegrade.py eyegrade/qtgui/gui.py eyegrade/qtgui/export.py"
+srcfiles="eyegrade/eyegrade.py eyegrade/qtgui/gui.py eyegrade/qtgui/export.py \
+          eyegrade/qtgui/dialogs.py eyegrade/qtgui/widgets.py \
+          eyegrade/qtgui/wizards.py eyegrade/qtgui/__init__.py"
 
 languages=$*
 if [ "x$languages" == "x" ]
