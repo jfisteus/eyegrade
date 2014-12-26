e0972cb4e3f9e5754a952acdf06bbc12247e7413
Jesus Arias Fisteus <jfisteus@gmail.com>
2014-12-27 00:48:27 +0100

    No need to enqueue the saving of captures in the event loop.

--- eyegrade/eyegrade.py
+++ eyegrade/eyegrade.py
@@ -271,7 +271,7 @@ class ProgramManager(object):
         if not self.exam.decisions.model:
             self.interface.enable_manual_detect(True)
         if self.mode.in_grading():
-            self.interface.run_later(self._store_capture_and_add)
+            self._store_capture_and_add()
 
     def _start_auto_change_detection(self):
         if not self.from_manual_detection:
@@ -606,7 +606,7 @@ class ProgramManager(object):
                                           self.exam.capture,
                                           self.exam.decisions,
                                           store_captures=False)
-            self.interface.run_later(self._store_capture_and_update)
+            self._store_capture_and_update()
 
     def _action_camera_selection(self):
         """Callback for opening the camera selection dialog."""
@@ -676,7 +676,7 @@ class ProgramManager(object):
                                          self.exam.capture,
                                          self.exam.decisions, self.exam.score,
                                          store_captures=False)
-            self.interface.run_later(self._store_capture_and_update)
+            self._store_capture_and_update()
 
     def _mouse_pressed_manual_detection(self, point):
         manager = self.manual_detect_manager
