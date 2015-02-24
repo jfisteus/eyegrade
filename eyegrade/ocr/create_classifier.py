58f1f1508e7d0b14c6f663f5959204e2662bfcb8
Jesus Arias Fisteus <jfisteus@gmail.com>
2015-02-24 01:24:43 +0100

    Adapt code style at the new ocr module.

--- eyegrade/ocr/create_classifier.py
+++ eyegrade/ocr/create_classifier.py
@@ -1,140 +1,141 @@
-import cv2, sys
+# Eyegrade: grading multiple choice questions with a webcam
+# Copyright (C) 2015 Rodrigo Arguello
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
+import sys
+import os
+import itertools
 import numpy as np
-from os import walk, path
+import cv2
 
 from . import preprocessing as imp
 
-#################################################################################
-#########################DATASET LOADING FUNCTIONS###############################
-#################################################################################
 
-def count(n,d):
+def count(n, d):
     count=0
     for i in d:
         if i==n:
             count+=1
     return count
 
-'''
-This function obtains the label associated to an image.
-'''
-def get_label(image_name, labels_file):
-	try:
-		with open(labels_file) as f:
-			for line_number, line in enumerate(f,1):
-				word = line.strip()
-				if image_name in word:
-					return word.split('\t')[1]
-	except e:
-		print 'Error:', e
-		return -1
+def load_labels(labels_file):
+    """Get the labels of all the images as a dictionary."""
+    labels = {}
+    with open(labels_file) as f:
+        for line in f:
+            parts = line.strip().split('\t')
+            labels[parts[0]] = parts[1]
+    return labels
 
-'''
-This function loads the dataset prepared for the classifier.
-It receives de folder with the images and a text file with all
-the labels for the digits.
-'''
-def load_dataset(folder, labels):
+def load_dataset(folder, labels_dict):
+    """Loads the dataset and prepares it for the classifier.
+
+    It receives de folder with the images and a text file with all
+    the labels for the digits.
+
+    """
     f = []
-    for (dirpath, dirnames, filenames) in walk(folder):
+    for (dirpath, dirnames, filenames) in os.walk(folder):
         f.extend(filenames)
         break
-
     m = len(f)
-
-    #Initialize empty training set
-    training_data = np.ndarray(shape=(m,imp.SZ*imp.SZ), dtype='float32')
-    training_labels = np.ndarray(shape=(m,1), dtype='float32')
-
+    # Initialize an empty training set:
+    training_data = np.ndarray(shape=(m, imp.SZ * imp.SZ), dtype='float32')
+    training_labels = np.ndarray(shape=(m, 1), dtype='float32')
     for index, filename in enumerate(f):
-        try:
-            if folder[-1] == '/':
-                img = cv2.imread(folder + filename, 0)
-            else:
-                img = cv2.imread(folder + '/' + filename, 0)
-            sample = imp.image_preprocessing(img)
-            training_data[index] = sample
-            training_labels[index] = float(get_label(filename, labels))
-        except:
-            print "Error con '%s'" % filename
-
+        if folder[-1] == '/':
+            img = cv2.imread(folder + filename, 0)
+        else:
+            img = cv2.imread(folder + '/' + filename, 0)
+        sample = imp.image_preprocessing(img)
+        training_data[index] = sample
+        training_labels[index] = float(labels_dict[filename])
     print 'Displaying distribution of the digits in the dataset...'
     for i in range(10):
         print i, '->', count(i,training_labels)
-
     return (training_data, training_labels)
 
-
-#################################################################################
-#########################CLASSIFIER CREATION FUNCTIONS###########################
-#################################################################################
-try:
-    digits_folder = sys.argv[1]
-    labels_file = sys.argv[2]
-except:
-    print 'Usage: python create_classifier.py [digits_folder] [labels_textfile]'
-    sys.exit(0)
-
-'''
-Function that tests a large range of parammeters for the SVM classifier
-and returns the one with optimal results. Only use this function the first
-time you are testing the best parameters for the SVM.
-'''
 def test_parameters(x_train, y_train, x_test, y_test):
+    """Tests a large range of parammeters for the SVM classifier.
 
+    Returns the one with optimal results.
+    Use this function only the first
+    time you are testing the best parameters for the SVM.
+
+    """
     scores = []
-    C=[]
-    gamma=[]
-    for i in range(21): C.append(10.0**(i-5))
-    for i in range(17): gamma.append(10**(i-14))
-
+    C = []
+    gamma = []
+    for i in range(21):
+        C.append(10.0 ** (i - 5))
+    for i in range(17):
+        gamma.append(10 ** (i - 14))
     parameters = list(itertools.product(C, gamma))
     for param in parameters:
-
-        svm_params = dict( kernel_type = cv2.SVM_RBF,
-            svm_type = cv2.SVM_C_SVC,
-            C=param[0], gamma=param[1])
-
+        svm_params = {
+            'kernel_type': cv2.SVM_RBF,
+            'svm_type': cv2.SVM_C_SVC,
+            'C': param[0],
+            'gamma': param[1],
+        }
         svm = cv2.SVM()
-        svm.train(x_train,y_train, params=svm_params)
+        svm.train(x_train, y_train, params=svm_params)
         result = svm.predict_all(x_test)
-        mask = result==y_test
+        mask = (result == y_test)
         correct = np.count_nonzero(mask)
-        score = correct*100.0/result.size
-
+        score = correct * 100.0 / result.size
         scores.append(score)
-        print 'C: %s, Gamma: %s, Score: %s' % (str(param[0]), str(param[1]), score)
-
+        print 'C: {1}, Gamma: {2}, Score: {3}'\
+          .format(param[0], param[1], score)
     max_score = max(scores)
     index_max = scores.index(max(scores))
     optimal = parameters[index_max]
     return (optimal, max_score)
 
-'''
-Main function for creating the SVM classifier
-'''
-def create_SVM(C=10, gamma=0.01, classifier_name='ocr_svm.dat'):
+def create_SVM(digits_folder, labels_file,
+               C=10, gamma=0.01, classifier_name='ocr_svm.dat.gz'):
+    """Creates the SVM classifier."""
     print 'Loading dataset...'
-    dataset, labels = load_dataset(digits_folder, labels_file)
+    labels_dict = load_labels(labels_file)
+    dataset, labels = load_dataset(digits_folder, labels_dict)
     print 'Dataset loaded succesfully!'
-
-    #100% of images are for training
+    # All the images are for training
     trainData = dataset[:][:]
     trainLabels = labels[:][:]
-
-    m = trainData.shape[0] # Total size
-    n = trainData.shape[1] # Number of features
-
+    ## m = trainData.shape[0] # Total size
+    ## n = trainData.shape[1] # Number of features
     svm_params = dict(kernel_type = cv2.SVM_RBF,
-                    svm_type = cv2.SVM_C_SVC, C=C, gamma=gamma)
-
+                      svm_type = cv2.SVM_C_SVC, C=C, gamma=gamma)
     print 'Creating classifier...'
     svm = cv2.SVM()
     svm.train(trainData, trainLabels, params=svm_params)
     svm.save(classifier_name)
     print 'Classifier created succesfully and saved as %s!' % classifier_name
-
     return svm
 
-create_SVM()
+def main():
+    try:
+        digits_folder = sys.argv[1]
+        labels_file = sys.argv[2]
+    except:
+        print ('Usage: python create_classifier.py digits_folder '
+               'labels_textfile')
+        sys.exit(0)
+    create_SVM(digits_folder, labels_file)
 
+if __name__ == '__main__':
+    main()
--- eyegrade/ocr/preprocessing.py
+++ eyegrade/ocr/preprocessing.py
@@ -1,5 +1,5 @@
 # Eyegrade: grading multiple choice questions with a webcam
-# Copyright (C) 2010-2015 Rodrigo Arguello
+# Copyright (C) 2015 Rodrigo Arguello
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,87 +19,80 @@
 import cv2
 import numpy as np
 
-#################################################################################
-#########################IMAGE PREPROCESSING FUNCTIONS###########################
-#################################################################################
-
 SZ = 28 #image size
 
-'''
-Function for deskewing an image (improves classifier performance).
-The image must be a cv2 image.
-'''
 def deskew(image):
-    affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
+    """Deskew an image.
+
+    It improves classifier performance.
+    The image must be a cv2 image.
+
+    """
+    affine_flags = cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR
     m = cv2.moments(image)
     if abs(m['mu02']) < 1e-2:
         return image.copy()
-    skew = m['mu11']/m['mu02']
-    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
-    image = cv2.warpAffine(image,M,(SZ, SZ),flags=affine_flags)
+    skew = m['mu11'] / m['mu02']
+    M = np.float32([[1, skew, -0.5 * SZ * skew], [0, 1, 0]])
+    image = cv2.warpAffine(image, M, (SZ, SZ), flags=affine_flags)
     return image
 
-'''
-Function for clearing blank surrounding area from an image 
-(improves classifier performance).
-The image must be a cv2 image.
-'''
 def clear_boundbox(image):
-    size_X = image.shape[0]
-    size_Y = image.shape[1]
+    """Clear the blank surrounding area of an image.
 
+    It improves classifier performance.
+    The image must be a cv2 image.
+
+    """
     top = 0
     bot = image.shape[0]
     right = image.shape[1]
     left = 0
-
     it = 0
-
-    for index,row in enumerate(image):
+    for index, row in enumerate(image):
         if (not np.all(row==0)) and it == 0:
-            if not np.all(image[index+1]==0):
+            if not np.all(image[index + 1] == 0):
                 top = index
                 it = 1
-        elif np.all(row==0) and it == 1:
+        elif np.all(row == 0) and it == 1:
             bot = index
             break
     it = 0
-
-    for index,col in enumerate(image.T):
-        if (not np.all(col==0)) and it == 0:
-            if not np.all(image.T[index+2]==0):
+    for index, col in enumerate(image.T):
+        if (not np.all(col == 0)) and it == 0:
+            if not np.all(image.T[index + 2] == 0):
                 left = index
                 it = 1
-        elif np.all(col==0) and it == 1:
+        elif np.all(col == 0) and it == 1:
             right = index
             break
-
     cleared_image = image[top:bot, left:right]
     return cleared_image
 
-'''
-Function ussing all previously defined image operations and returning
-a numpy feature vector ready to be introduced to train the classifier.
-The function receives a cv2 image.
-'''
 def image_preprocessing(image):
-	image = deskew(image)
-	image = clear_boundbox(image)
-	image = cv2.resize(image, (SZ, SZ))
-	image_matrix = np.array(image, np.float32)/255.0
-	feature_vector = image_matrix.reshape(SZ*SZ,)
-	return feature_vector
+    """Preprocesses the image of a digit.
 
-'''
-import matplotlib.pyplot as plt
+    Returns a numpy feature vector ready to be introduced
+    to train the classifier.
 
-def draw(matrix1, matrix2 = None):
-    plt.subplot(2, 4, 1)
-    plt.axis('off')
-    plt.imshow(matrix1, cmap=plt.cm.gray_r, interpolation='nearest')
-    if matrix2 != None:
-        plt.subplot(2, 4, 2)
-        plt.axis('off')
-        plt.imshow(matrix2, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.show()
-'''
+    The function receives a cv2 image.
+
+    """
+    image = deskew(image)
+    image = clear_boundbox(image)
+    image = cv2.resize(image, (SZ, SZ))
+    image_matrix = np.array(image, np.float32)/255.0
+    feature_vector = image_matrix.reshape(SZ*SZ,)
+    return feature_vector
+
+
+## import matplotlib.pyplot as plt
+## def draw(matrix1, matrix2 = None):
+##     plt.subplot(2, 4, 1)
+##     plt.axis('off')
+##     plt.imshow(matrix1, cmap=plt.cm.gray_r, interpolation='nearest')
+##     if matrix2 != None:
+##         plt.subplot(2, 4, 2)
+##         plt.axis('off')
+##         plt.imshow(matrix2, cmap=plt.cm.gray_r, interpolation='nearest')
+##     plt.show()
