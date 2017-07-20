b13f2ffba9714b405b9b8395df358dd80fc2f972
Jesus Arias Fisteus <jfisteus@gmail.com>
2017-07-20 19:04:15 +0200

    Update version number and ChangeLog for release 0.7

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ to work properly:
 """
 
 setuptools.setup(name='eyegrade',
-      version='0.7rc2',
+      version='0.7',
       description='Grade MCQ exams with a webcam',
       long_description=long_description,
       author='Jesus Arias Fisteus',
