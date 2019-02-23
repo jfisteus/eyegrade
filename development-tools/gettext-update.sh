9fc73ee501890a0dbd76216c7ea768589acba3bf
Jesus Arias Fisteus <jfisteus@gmail.com>
2019-02-23 08:54:00 +0100

    Update translations

--- development-tools/gettext-update.sh
+++ development-tools/gettext-update.sh
@@ -2,7 +2,8 @@
 
 srcfiles="eyegrade/eyegrade.py eyegrade/qtgui/gui.py eyegrade/qtgui/export.py \
           eyegrade/qtgui/dialogs.py eyegrade/qtgui/widgets.py \
-          eyegrade/qtgui/wizards.py eyegrade/qtgui/__init__.py"
+          eyegrade/qtgui/wizards.py eyegrade/qtgui/__init__.py \
+          eyegrade/qtgui/students.py"
 
 languages=$*
 if [ "x$languages" == "x" ]
