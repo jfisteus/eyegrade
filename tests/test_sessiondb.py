jfisteus/eyegrade

commit 385d2c48e4cee52a7592118f067d2156ffd915c5

Fix some issues detected by pylint and flake8
Other issues remain.

eyegrade/create_exam.py
@@ -283,7 +283,7 @@ def create_exam():
     else:
         exam = None
         if options.dimensions is not None:
-            dimensions, num_options = utils.parse_dimensions(options.dimensions, True)
+            dimensions, _ = utils.parse_dimensions(options.dimensions, True)
             num_choices = dimensions[0][0]
             num_questions = sum([d[1] for d in dimensions])
             if options.num_choices is not None and options.num_choices != num_choices:

eyegrade/exammaker.py
@@ -599,10 +599,10 @@ def _table_top(num_tables, num_choices, compact, table_width=None, table_height=
     )
     lines.append(r"\begin{tabular}{" + column_flags + "}")
     parts = []
-    for i in range(0, num_tables):
+    for _ in range(num_tables):
         parts_internal = []
         parts_internal.append(r"\multicolumn{1}{c}{}")
-        for j in range(0, num_choices):
+        for j in range(num_choices):
             parts_internal.append(r"\multicolumn{1}{c}{%s}" % chr(65 + j))
         parts.append(" & ".join(parts_internal))
     lines.append(middle_sep_header.join(parts) + r" \\")
@@ -683,7 +683,7 @@ def format_questions(exam, model, with_solution=False):
         data.append("\\end{enumerate}\n")
         if model != "0":
             data.append("\n\n% solutions: ")
-            solutions, permutations = exam.solutions_and_permutations(model)
+            solutions, _ = exam.solutions_and_permutations(model)
             data.append(" ".join([str(n) for n in solutions]))
             data.append("\n")
     return "".join(data)

eyegrade/exams.py
@@ -725,7 +725,7 @@ def shuffle(data):
     to_sort = [(random.random(), item, pos) for pos, item in enumerate(data)]
     shuffled_data = []
     permutations = []
-    for val, item, pos in sorted(to_sort):
+    for _, item, pos in sorted(to_sort):
         shuffled_data.append(item)
         permutations.append(pos)
     return shuffled_data, permutations

eyegrade/qtgui/wizards.py
@@ -325,7 +325,7 @@ class NewSessionPageExamAnswers(QWizardPage):
                 mygroupboxCol = QGroupBox()
                 myformCol = QFormLayout()
                 mygroupboxCol.setLayout(myformCol)
-                for y in range(int(col)):
+                for _ in range(int(col)):
                     ansID += 1
                     radioGroupList[ansID] = QButtonGroup()
                     layoutRow = QHBoxLayout()
@@ -525,26 +525,20 @@ class NewSessionPageScores(QWizardPage):
     def _update_combo(self, new_index):
         if new_index != self.current_mode:
             # Ask the user if changes to weights may be lost
-            if self.current_mode == 2 and self.weights_table.model().changes:
-                if not self._show_warning_weights_reset():
-                    self.combo.setCurrentIndex(self.current_mode)
-                    return
+            if (
+                self.current_mode == 2
+                and self.weights_table.model().changes
+                and not self._show_warning_weights_reset()
+            ):
+                self.combo.setCurrentIndex(self.current_mode)
+                return
             self.button_reset.setEnabled(False)
             if new_index == 0:
-                for widget in self.base_score_widgets:
-                    widget.setEnabled(False)
-                for widget in self.weights_widgets:
-                    widget.setEnabled(False)
+                self._enable_weights_widgets(False, False)
             elif new_index == 1:
-                for widget in self.base_score_widgets:
-                    widget.setEnabled(True)
-                for widget in self.weights_widgets:
-                    widget.setEnabled(False)
+                self._enable_weights_widgets(True, False)
             else:
-                for widget in self.base_score_widgets:
-                    widget.setEnabled(True)
-                for widget in self.weights_widgets:
-                    widget.setEnabled(True)
+                self._enable_weights_widgets(True, True)
             # Reset the weights table
             if self.current_mode == 2:
                 self.weights_table.model().clear()
@@ -554,6 +548,12 @@ class NewSessionPageScores(QWizardPage):
             if new_index == 0:
                 self.clear_base_scores()
             self.current_mode = new_index
+
+    def _enable_weights_widgets(self, enable_base_scores, enable_weights):
+        for widget in self.base_score_widgets:
+            widget.setEnabled(enable_base_scores)
+        for widget in self.weights_widgets:
+            widget.setEnabled(enable_weights)
 
     def _reset_weights(self):
         if self.weights_table.model().changes and self._show_warning_weights_reset():

commit 342af1c2abec75f10b3dabfea221e51159dd9f9e

Add some unit tests for sessiondb and other fixes
Fixed also a bug introduced by commit 385d2c48e4cee52a7592118f067d2156ffd915c5
(throw-away variables cannot be named "_" because they interfere
with gettext).

eyegrade/create_exam.py
@@ -283,7 +283,7 @@ def create_exam():
     else:
         exam = None
         if options.dimensions is not None:
-            dimensions, _ = utils.parse_dimensions(options.dimensions, True)
+            dimensions, __ = utils.parse_dimensions(options.dimensions, True)
             num_choices = dimensions[0][0]
             num_questions = sum([d[1] for d in dimensions])
             if options.num_choices is not None and options.num_choices != num_choices:

eyegrade/exammaker.py
@@ -599,7 +599,7 @@ def _table_top(num_tables, num_choices, compact, table_width=None, table_height=
     )
     lines.append(r"\begin{tabular}{" + column_flags + "}")
     parts = []
-    for _ in range(num_tables):
+    for __ in range(num_tables):
         parts_internal = []
         parts_internal.append(r"\multicolumn{1}{c}{}")
         for j in range(num_choices):
@@ -683,7 +683,7 @@ def format_questions(exam, model, with_solution=False):
         data.append("\\end{enumerate}\n")
         if model != "0":
             data.append("\n\n% solutions: ")
-            solutions, _ = exam.solutions_and_permutations(model)
+            solutions, __ = exam.solutions_and_permutations(model)
             data.append(" ".join([str(n) for n in solutions]))
             data.append("\n")
     return "".join(data)

eyegrade/exams.py
@@ -187,6 +187,28 @@ class ExamConfig:
         else:
             self.capture_pattern = utils.default_capture_pattern
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            equal = self.as_tuple() == other.as_tuple()
+        else:
+            equal = False
+        return equal
+
+    def as_tuple(self):
+        return (
+            self.num_questions,
+            self.solutions,
+            self.id_num_digits,
+            self.dimensions,
+            self.permutations,
+            self.models,
+            self.scores,
+            self.base_scores,
+            self.left_to_right_numbering,
+            self.survey_mode,
+            self.scores_mode,
+        )
+
     def add_model(self, model):
         if model not in self.models:
             self.models.append(model)
@@ -367,7 +389,7 @@ class ExamConfig:
             raise ValueError("Scores with an incorrect number of questions")
         if self.scores and sorted(scores) != sorted(next(iter(self.scores.values()))):
             #        if self.scores and sorted(scores) != sorted(self.scores.values()[0]):
-            raise ValueError("Scores for all models must be equal " "but their order")
+            raise ValueError("Scores for all models must be equal but their order")
         self.scores[model] = scores
         self.add_model(model)
 
@@ -454,7 +476,7 @@ class ExamConfig:
             self.scores_mode = ExamConfig.SCORES_MODE_NONE
         else:
             raise Exception(
-                "Exam config must contain correct and incorrect " "weight or none"
+                "Exam config must contain correct and incorrect weight or none"
             )
         if exam_data.has_option("exam", "left-to-right-numbering"):
             self.left_to_right_numbering = exam_data.getboolean(
@@ -725,7 +747,7 @@ def shuffle(data):
     to_sort = [(random.random(), item, pos) for pos, item in enumerate(data)]
     shuffled_data = []
     permutations = []
-    for _, item, pos in sorted(to_sort):
+    for __, item, pos in sorted(to_sort):
         shuffled_data.append(item)
         permutations.append(pos)
     return shuffled_data, permutations

eyegrade/eyegrade.py
@@ -507,7 +507,7 @@ class ProgramManager:
                 return
         if self.mode.in_grading() or self.mode.in_review_from_session():
             self._stop_grading()
-        self.sessiondb.save_legacy_answers(self.config["csv-dialect"])
+        self.sessiondb.save_legacy_answers()
         self.sessiondb.close()
         self.mode.enter_no_session()
         self.sessiondb = None
@@ -518,7 +518,7 @@ class ProgramManager:
     def _exit_application(self):
         """Callback for when the user wants to exit the application."""
         if self.mode.in_manual_detect():
-            exit = self.interface.show_warning(
+            exit_ = self.interface.show_warning(
                 _(
                     "The current capture has not been saved and will be lost. "
                     "Are you sure you want to exit the application?"
@@ -527,13 +527,13 @@ class ProgramManager:
             )
         elif self.mode.in_grading() or self.mode.in_review_from_session():
             self._stop_grading()
-            exit = True
+            exit_ = True
         else:
-            exit = True
+            exit_ = True
         if exit and self.sessiondb is not None:
-            self.sessiondb.save_legacy_answers(self.config["csv-dialect"])
+            self.sessiondb.save_legacy_answers()
             self.sessiondb.close()
-        return exit
+        return exit_
 
     def _action_start(self):
         if self.mode.in_review_from_session():

eyegrade/qtgui/wizards.py
@@ -325,7 +325,7 @@ class NewSessionPageExamAnswers(QWizardPage):
                 mygroupboxCol = QGroupBox()
                 myformCol = QFormLayout()
                 mygroupboxCol.setLayout(myformCol)
-                for _ in range(int(col)):
+                for __ in range(int(col)):
                     ansID += 1
                     radioGroupList[ansID] = QButtonGroup()
                     layoutRow = QHBoxLayout()

eyegrade/sessiondb.py
@@ -175,7 +175,7 @@ class SessionDB:
         self.schema_version = self._check_schema()
         self.exam_config = self._load_exam_config()
         self._compute_num_questions_and_choices()
-        self.capture_save_func = None
+        self.capture_save_func = lambda name: None
         self._student_listings = None
 
     @property
@@ -255,7 +255,7 @@ class SessionDB:
         listing = self.student_listings.listing_by_group_id(student.group_id)
         listing.add_students((student,))
 
-    def _store_students(self, student_list, commit=True):
+    def store_students(self, student_list, commit=True):
         cursor = self.conn.cursor()
         data = []
         # Insert one by one with execute() instead of using executemany()
@@ -389,7 +389,7 @@ class SessionDB:
         else:
             return 1
 
-    def save_legacy_answers(self, csv_dialect):
+    def save_legacy_answers(self):
         file_name = os.path.join(self.session_dir, "eyegrade-answers.csv")
         file_format = export.FileFormat.CSV_TABS
         with export.create_writer(file_name, file_format) as writer:
@@ -709,9 +709,7 @@ class SessionDB:
             scores = {}
         models = []
         model = None
-        for row in cursor.execute(
-            "SELECT * FROM Questions " "ORDER BY model, question"
-        ):
+        for row in cursor.execute("SELECT * FROM Questions ORDER BY model, question"):
             if row["model"] != model:
                 model = row["model"]
                 models.append(model)
@@ -732,10 +730,10 @@ class SessionDB:
             if scores_mode == exams.ExamConfig.SCORES_MODE_WEIGHTS:
                 model_weights.append(row["score_weight"])
             elif scores_mode == exams.ExamConfig.SCORES_MODE_INDIVIDUAL:
-                model_scores = scoring.QuestionScores(
+                question_scores = scoring.QuestionScores(
                     row["score_correct"], row["score_incorrect"], row["score_blank"]
                 )
-                model_scores.append(scores)
+                model_scores.append(question_scores)
         for m in models:
             model = _Adapter.dec_model(m)
             if solutions[m][0] is not None:
@@ -924,7 +922,7 @@ class GroupListingFromDB(students.GroupListing):
 
     def add_students(self, student_list):
         super().add_students(student_list)
-        self.sessiondb._store_students(student_list)
+        self.sessiondb.store_students(student_list)
 
     def rename(self, new_name):
         super().rename(new_name)

eyegrade/students.py
@@ -121,7 +121,7 @@ class StudentGroup:
 class GroupListing:
     def __init__(self, group, students):
         self.group = group
-        self.students = students
+        self.students = list(students)
         self.parent = None
         self._students_dict = {s.student_id: s for s in students}
 
@@ -129,7 +129,7 @@ class GroupListing:
         return self._students_dict.get(student_id, None)
 
     def add_students(self, students):
-        if len(students) > 0:
+        if students:
             if self.parent is not None:
                 duplicates = self.parent.find_duplicates(students)
             else:
@@ -279,6 +279,8 @@ class StudentReader:
     def __init__(self, file_name, column_map=None):
         self.file_name = file_name
         self.column_map = column_map
+        # To be overwritten by subclasses:
+        self.iterator = iter([])
 
     @staticmethod
     def create(file_name):

tests/test-weights.eye
@@ -1,0 +1,14 @@
+[exam]
+dimensions: 3,5
+id-num-digits: 8
+correct-weight: 5/4
+incorrect-weight: 0
+blank-weight: 0
+
+[solutions]
+model-A: 1/2/1/3/2
+model-B: 3/2/2/2/1
+
+[question-score-weights]
+weights-A: 1,2,1,3,1
+weights-B: 1,1,2,1,3

tests/test.eye
@@ -1,0 +1,18 @@
+[exam]
+dimensions: 3,5
+id-num-digits: 9
+correct-weight: 4/5
+incorrect-weight: 2/5
+blank-weight: 0
+
+[solutions]
+model-A: 3/2/3/3/1
+model-B: 2/1/2/2/1
+model-C: 2/1/1/1/2
+model-D: 1/2/2/1/2
+
+[permutations]
+permutations-A: 1{3,2,1}/3{3,1,2}/4{2,3,1}/2{3,2,1}/5{1,2,3}
+permutations-B: 2{2,1,3}/4{1,3,2}/3{2,1,3}/1{2,1,3}/5{1,3,2}
+permutations-C: 1{2,1,3}/4{1,2,3}/3{1,2,3}/5{1,2,3}/2{2,1,3}
+permutations-D: 5{1,2,3}/2{2,1,3}/4{2,1,3}/3{1,2,3}/1{3,1,2}

tests/test_sessiondb.py
@@ -1,0 +1,98 @@
+# Eyegrade: grading multiple choice questions with a webcam
+# Copyright (C) 2010-2019 Jesus Arias Fisteus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+import os.path
+import tempfile
+
+import eyegrade.sessiondb as sessiondb
+import eyegrade.exams as exams
+import eyegrade.students as students
+
+
+class TestSessionDB(unittest.TestCase):
+    def setUp(self):
+        self.students = [
+            students.Student("101010101", "Donald Duck", "", "", ""),
+            students.Student("202020202", "Marty McFly", "", "", ""),
+            students.Student("313131313", "Peter Pan", "", "", ""),
+        ]
+        self.more_students = [
+            students.Student("909090909", "Donkey Kong", "", "", ""),
+            students.Student("818181818", "Pinoccio", "", "", ""),
+            students.Student("555555555", "Gump", "", "", ""),
+        ]
+
+    def _get_test_file_path(self, filename):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(dirname, filename)
+
+    def test_exam_data(self):
+        exam_config = exams.ExamConfig(filename=self._get_test_file_path("test.eye"))
+        exam_config.capture_pattern = "exam-{student-id}-{seq-number}.png"
+        listing = students.GroupListing(students.StudentGroup(1, "G"), [])
+        listing.add_students(self.students)
+        listings = students.StudentListings()
+        listings.add_listing(listing)
+        with tempfile.TemporaryDirectory() as dir_name:
+            session_dir = os.path.join(dir_name, "test_session")
+            sessiondb.create_session_directory(session_dir, exam_config, listings)
+            session = sessiondb.SessionDB(session_dir)
+            self.assertEqual(session.exam_config, exam_config)
+
+    def test_exam_data_weights(self):
+        exam_config = exams.ExamConfig(
+            filename=self._get_test_file_path("test-weights.eye")
+        )
+        exam_config.capture_pattern = "exam-{student-id}-{seq-number}.png"
+        listing = students.GroupListing(students.StudentGroup(1, "G"), [])
+        listing.add_students(self.students)
+        listings = students.StudentListings()
+        listings.add_listing(listing)
+        with tempfile.TemporaryDirectory() as dir_name:
+            session_dir = os.path.join(dir_name, "test_session")
+            sessiondb.create_session_directory(session_dir, exam_config, listings)
+            session = sessiondb.SessionDB(session_dir)
+            self.assertEqual(session.exam_config, exam_config)
+
+    def test_student_list(self):
+        exam_config = exams.ExamConfig(filename=self._get_test_file_path("test.eye"))
+        exam_config.capture_pattern = "exam-{student-id}-{seq-number}.png"
+        listing = students.GroupListing(students.StudentGroup(1, "G"), [])
+        listing.add_students(self.students)
+        listings = students.StudentListings()
+        listings.add_listing(listing)
+        with tempfile.TemporaryDirectory() as dir_name:
+            session_dir = os.path.join(dir_name, "test_session")
+            sessiondb.create_session_directory(session_dir, exam_config, listings)
+            session = sessiondb.SessionDB(session_dir)
+            session.student_listings[0].add_students(self.more_students)
+            session.close()
+            session = sessiondb.SessionDB(session_dir)
+            student_list = list(session.student_listings[0])
+            self.assertEqual(
+                len(student_list), len(self.students) + len(self.more_students)
+            )
+            for student_1, student_2 in zip(self.students, student_list):
+                self.assertEqual(student_1.name, student_2.name)
+                self.assertEqual(student_1.student_id, student_2.student_id)
+            for student_1, student_2 in zip(
+                self.more_students, student_list[len(self.students) :]
+            ):
+                self.assertEqual(student_1.name, student_2.name)
+                self.assertEqual(student_1.student_id, student_2.student_id)