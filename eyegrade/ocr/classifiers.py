00a722d41c36d1185451371481a73a8bfd0fb4da
Jesus Arias Fisteus <jfisteus@gmail.com>
2021-11-25 23:48:21 +0100

    Update copyright year to 2021

--- eyegrade/ocr/classifiers.py
+++ eyegrade/ocr/classifiers.py
@@ -1,5 +1,5 @@
 # Eyegrade: grading multiple choice questions with a webcam
-# Copyright (C) 2010-2018 Rodrigo Arguello, Jesus Arias Fisteus
+# Copyright (C) 2010-2021 Rodrigo Arguello, Jesus Arias Fisteus
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
