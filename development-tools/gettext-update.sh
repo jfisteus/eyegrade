6e3c4be2ef34fee2ff0a140eb355d5b8094f5e41
Jesus Arias Fisteus <jfisteus@gmail.com>
2025-11-25 13:23:40 +0100

    Update translations (again)
    A couple of files with translatable strings were missing
    from the translations of commit cf05cabc9bc9fd.

--- development-tools/gettext-update.sh
+++ development-tools/gettext-update.sh
@@ -3,7 +3,8 @@
 srcfiles="eyegrade/eyegrade.py eyegrade/qtgui/gui.py eyegrade/qtgui/export.py \
           eyegrade/qtgui/dialogs.py eyegrade/qtgui/widgets.py \
           eyegrade/qtgui/wizards.py eyegrade/qtgui/__init__.py \
-          eyegrade/qtgui/students.py"
+          eyegrade/qtgui/students.py eyegrade/qtgui/scores.py \
+          eyegrade/qtgui/show_stats.py"
 
 languages=$*
 if [ "x$languages" == "x" ]
